ca93b0fc820d50a52706163d3637821e3677b95f
桑明明 <msang@xiaohongshu.com>
2017-01-21 19:48:06 +0800

    添加新的云函数

--- cloud.js
+++ cloud.js
@@ -1,5 +1,15 @@
 var AV = require('leanengine');
 
-AV.Cloud.define('hello', function(req, res){console.log(req);res.success('Hello world in cloud function');})
+AV.Cloud.define('hello', function(req, res){
+    console.log(req);
+    res.success('Hello world in cloud function');
+});
+
+AV.Cloud.beforeSave('Review', function(request, response) {
+    response.error(JSON.stringify({
+        code: 123,
+        message: '自定义错误信息'
+    }));
+});
 
 module.exports = AV.Cloud;
