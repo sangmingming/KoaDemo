9e04e167199b08f60de1b85de65f0c6f4930289b
桑明明 <msang@xiaohongshu.com>
2017-01-21 20:45:32 +0800

    添加存储测试

--- cloud.js
+++ cloud.js
@@ -1,9 +1,43 @@
 var AV = require('leanengine');
+var http = require('http');
+var cheerio = require('cheerio');
 
 AV.Cloud.define('hello', function(req, res){
     console.log(req);
     res.success('Hello world in cloud function');
 });
+
+AV.Cloud.define('doubanMusic', function(req, res) {
+    http.get("https://music.douban.com/chart", function(response) {
+        var html = "";
+        response.on('data', function(data) {
+            html += data;
+        });
+
+        response.on('end', function() {
+            callback(html);
+        });
+    });
+} );
+
+var DoubanMusic = AV.Object.extend("DoubanMusic");
+
+function callback(html) {
+    var $ = cheerio.load(html);
+    var result = [];
+
+    $("ul.col5 li.clearfix").each(function(index, element) {
+        var url = $(element).find("a.face").attr("href");
+        result.push('url');
+        var doubanMusic = new DoubanMusic();
+        doubanMusic.save({
+            "icon": url
+        }).then(function(object) {
+            console.log("success");
+        });
+    
+    })
+}
 
 AV.Cloud.beforeSave('Review', function(request, response) {
     response.error(JSON.stringify({
