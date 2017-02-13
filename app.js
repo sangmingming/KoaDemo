53a249c8d8840da88a7357db976bad3aad6b0f6d
桑明明 <msang@xiaohongshu.com>
2017-02-13 20:06:40 +0800

    add convert

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 var koa = require('koa');
 var Router = require('koa-router');
-
+var convert = require('koa-convert');
 var AV = require('leanengine');
 
 var app = new koa();
@@ -14,7 +14,7 @@ router.get("/", function(ctx, next) {
 
 require("./cloud");
 
-app.use(AV.koa());
+app.use(convert(AV.koa()));
 app.use(router.routes())
     .use(router.allowedMethods());
 module.exports = app;
